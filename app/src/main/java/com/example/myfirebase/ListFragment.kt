7dc8cabefe1faa13842e12707773c50c77db4222
Johan Anderberg <johan_anderberg79@hotmail.com>
2020-10-30 00:07:32 +0100

    recyclerview 1

--- app/src/main/java/com/example/myfirebase/ListFragment.kt
+++ app/src/main/java/com/example/myfirebase/ListFragment.kt
@@ -1,18 +1,33 @@
 package com.example.myfirebase
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
+
+    var todoAdapter = TodoAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.w("johandebug", todoAdapter.todolist.toString())
+        taskRecView.layoutManager = LinearLayoutManager(context)
+        taskRecView.adapter = todoAdapter
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+
     }
+
 
 }
