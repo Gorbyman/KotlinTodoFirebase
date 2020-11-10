9406c2e543632e6e302ee76fbd909b02d699acb0
Johan Anderberg <johan_anderberg79@hotmail.com>
2020-11-10 23:46:02 +0100

    Loadtodo moved to onstart in listfragment

--- app/src/main/java/com/example/myfirebase/ListFragment.kt
+++ app/src/main/java/com/example/myfirebase/ListFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myfirebase
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,16 +8,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
     var todoAdapter = TodoAdapter()
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
         Log.w("johandebug", todoAdapter.todolist.toString())
     }
 
@@ -33,15 +39,15 @@ class ListFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         taskRecView.layoutManager = LinearLayoutManager(context)
         taskRecView.adapter = todoAdapter
-        if(todoAdapter.todolist.isNotEmpty()){
-            todoAdapter.loadTodo()
-        }
     }
 
     override fun onStart() {
         super.onStart()
         toAddTaskBtn.setOnClickListener {
             activity!!.supportFragmentManager.beginTransaction().replace(R.id.mainFragmentHolder, AddTaskFragment()).commit()
+        }
+        if(auth.currentUser != null){
+            todoAdapter.loadTodo()
         }
     }
 
